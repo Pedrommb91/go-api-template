74c28ada6e0a19667a682ec1ae0ee6fa75883636
pedro barreiro <p.barreiro@kigroup.de>
2023-05-10 08:56:22 +0100

    test: server and router tests

---
+++ internal/api/server_test.go
@@ -0,0 +1,65 @@
+package api
+
+import (
+	"net/http"
+	"os"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/Pedrommb91/go-api-template/config"
+	"github.com/Pedrommb91/go-api-template/pkg/logger"
+	"github.com/gin-gonic/gin"
+)
+
+func TestServer_Run(t *testing.T) {
+	handler := gin.New()
+	type fields struct {
+		cfg    *config.Config
+		log    *logger.Logger
+		engine *gin.Engine
+		server *http.Server
+	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		{
+			name: "valid args runs ok",
+			fields: fields{
+				cfg: &config.Config{
+					API: config.API{
+						Address:          ":0",
+						CORSAllowOrigins: []string{"https://ttt.com"},
+					},
+				},
+				log:    logger.New("info"),
+				engine: handler,
+				server: &http.Server{
+					Addr:              ":0",
+					Handler:           handler,
+					ReadHeaderTimeout: time.Second * 30,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := NewServer(tt.fields.cfg, tt.fields.log)
+			s.ServerConfigure()
+			s.SetRoutes()
+			go func() {
+				time.Sleep(time.Millisecond * 500)
+				p, err := os.FindProcess(os.Getpid())
+				if err != nil {
+					t.Errorf("Could not find process")
+				}
+				err = p.Signal(syscall.SIGINT)
+				if err != nil {
+					t.Errorf("Could not find process")
+				}
+			}()
+			s.Run()
+		})
+	}
+}
---
+++ internal/router/router_test.go
@@ -0,0 +1,38 @@
+package router
+
+import (
+	"testing"
+
+	"github.com/Pedrommb91/go-api-template/config"
+	"github.com/Pedrommb91/go-api-template/pkg/logger"
+	"github.com/gin-gonic/gin"
+)
+
+func TestNewRouter(t *testing.T) {
+	type args struct {
+		engine *gin.Engine
+		l      logger.Interface
+		cfg    *config.Config
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "New router success",
+			args: args{
+				engine: &gin.Engine{RouterGroup: gin.New().RouterGroup},
+				l:      logger.New("info"),
+				cfg:    &config.Config{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			NewRouter(tt.args.engine, tt.args.l, tt.args.cfg)
+			if len(tt.args.engine.Handlers) == 0 {
+				t.Errorf("Failed to register handlers")
+			}
+		})
+	}
+}
